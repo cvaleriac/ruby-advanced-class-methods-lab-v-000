98770204db81177acff8ba704c76fc5f9a0fbf4b
valeria campilongo <cvaleriac@hotmail.it>
2018-05-29 16:18:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,9 +33,9 @@ def self.find_by_name(name)
 end
 
 def self.find_or_create_by_name(name)
-  if self.find_by_name
+  if self.find_by_name(name)
     puts name
-  else self.create_by_name
+  else self.create_by_name(name)
   end
 end
 end
