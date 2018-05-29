7f1717594159a2dd973f579857aec1309962fb6e
valeria campilongo <cvaleriac@hotmail.it>
2018-05-29 13:46:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ end
 def self.find_or_create_by_name(name)
 self.find_by_name || self.create_by_name
 if @@all.include?(name)
-  @@all.find { |song| song.name == name }
+  @@all.find_by_name { |song| song.name == name }
 else
   name = self.create
   name
