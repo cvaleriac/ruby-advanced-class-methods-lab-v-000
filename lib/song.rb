c36c44566c92e2231a1f8c592f24d61acfd80cab
valeria campilongo <cvaleriac@hotmail.it>
2018-05-29 14:16:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,16 +34,5 @@ end
 
 def self.find_or_create_by_name(name)
 self.find_by_name || self.create_by_name
-
-def add_song(song)
-    @songs << song
-  end
-
-  def print_songs
-    self.songs.each do |song|
-      puts song.name
-    end
-  end
-
 end
 end
